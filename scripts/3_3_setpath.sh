65f5d4df20e6c636b51a38d52ac7b10553e8db5f
andrewcgaitskell <andrew@gaitskell.com>
2020-10-09 20:28:58 +0100

    Update 3_3_setpath.sh

--- scripts/3_3_setpath.sh
+++ scripts/3_3_setpath.sh
@@ -1,4 +1,5 @@
+#!/bin/sh
 VERSION=v12.19.0
 DISTRO=linux-x64
-export PATH=/usr/local/lib/nodejs/node-$VERSION-$DISTRO/bin:$PATH >> ~/.profile
+export PATH=/usr/local/lib/nodejs/node-$VERSION-$DISTRO/bin:$PATH
 echo $PATH

9f42a24061252f1b499dbbc4394f595c33d70836
andrewcgaitskell <andrew@gaitskell.com>
2020-10-09 21:25:46 +0100

    Update 3_1_install_node.sh

--- scripts/3_1_install_node.sh
+++ scripts/3_1_install_node.sh
@@ -1,7 +1,3 @@
-wget https://nodejs.org/dist//v12.19.0/node-v12.19.0-linux-x64.tar.gz
-VERSION=v12.19.0
-DISTRO=linux-x64
-sudo mkdir -p /usr/local/lib/nodejs
-tar -xzvf node-$VERSION-$DISTRO.tar.gz -C /usr/local/lib/nodejs 
-
-source 3_2_setpath.sh
+# Using Ubuntu
+curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+apt install -y nodejs

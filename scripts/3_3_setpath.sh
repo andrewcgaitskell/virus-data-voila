cabb60c48df7dcfe1bdff9d0caf13ae3e186b5a4
andrewcgaitskell <andrew@gaitskell.com>
2020-10-09 19:56:52 +0100

    Update 3_3_setpath.sh

--- scripts/3_3_setpath.sh
+++ scripts/3_3_setpath.sh
@@ -1,18 +1,4 @@
 VERSION=v12.19.0
 DISTRO=linux-x64
-
 export PATH=/usr/local/lib/nodejs/node-$VERSION-$DISTRO/bin:$PATH
-# Refresh profile
-. ~/.profile
-
-
-# Test installation using
-# node -v
-
-##npm version
-
-##npx -v
-
-####sudo apt install nodejs npm
-
-##sudo npm install -g configurable-http-proxy
+echo $PATH

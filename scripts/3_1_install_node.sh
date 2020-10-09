cf4f0c093fc2100f33520a350cc9ff9d30fd0799
andrewcgaitskell <andrew@gaitskell.com>
2020-10-09 19:30:55 +0100

    Update 3_1_install_node.sh

--- scripts/3_1_install_node.sh
+++ scripts/3_1_install_node.sh
@@ -1,4 +1,23 @@
-sudo apt install nodejs npm
 
-sudo npm install -g configurable-http-proxy
+VERSION=v12.19.0
+DISTRO=linux-x64
+sudo mkdir -p /usr/local/lib/nodejs
+sudo tar -xJvf node-$VERSION-$DISTRO.tar.xz -C /usr/local/lib/nodejs 
+## Set the environment variable ~/.profile, add below to the end
+# Nodejs
+VERSION=v12.19.0
+DISTRO=linux-x64
+export PATH=/usr/local/lib/nodejs/node-$VERSION-$DISTRO/bin:$PATH
+## Refresh profile
+. ~/.profile
+##Test installation using
+##node -v
+
+##npm version
+
+##npx -v
+
+####sudo apt install nodejs npm
+
+##sudo npm install -g configurable-http-proxy
 

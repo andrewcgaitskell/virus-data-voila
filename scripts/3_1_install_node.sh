6edb6043d0e4baf90e7bb1b28d1d84f02a52d65f
andrewcgaitskell <andrew@gaitskell.com>
2020-10-09 19:44:26 +0100

    Update 3_1_install_node.sh

--- scripts/3_1_install_node.sh
+++ scripts/3_1_install_node.sh
@@ -1,19 +1,15 @@
-
 wget https://nodejs.org/dist//v12.19.0/node-v12.19.0-linux-x64.tar.gz
-
 VERSION=v12.19.0
 DISTRO=linux-x64
 sudo mkdir -p /usr/local/lib/nodejs
 tar -xzvf node-$VERSION-$DISTRO.tar.gz -C /usr/local/lib/nodejs 
-## Set the environment variable ~/.profile, add below to the end
+# Set the environment variable ~/.profile, add below to the end
 # Nodejs
-VERSION=v12.19.0
-DISTRO=linux-x64
 export PATH=/usr/local/lib/nodejs/node-$VERSION-$DISTRO/bin:$PATH
-## Refresh profile
+# Refresh profile
 . ~/.profile
-##Test installation using
-##node -v
+# Test installation using
+# node -v
 
 ##npm version
 

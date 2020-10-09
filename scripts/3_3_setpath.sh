ddbf68e4e273624ba5b00a105d14f2fa26cf0129
andrewcgaitskell <andrew@gaitskell.com>
2020-10-09 19:47:58 +0100

    Create 3_3_setpath.sh

---
+++ scripts/3_3_setpath.sh
@@ -0,0 +1,17 @@
+
+
+export PATH=/usr/local/lib/nodejs/node-$VERSION-$DISTRO/bin:$PATH
+# Refresh profile
+. ~/.profile
+
+
+# Test installation using
+# node -v
+
+##npm version
+
+##npx -v
+
+####sudo apt install nodejs npm
+
+##sudo npm install -g configurable-http-proxy

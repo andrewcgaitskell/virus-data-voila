f7bec8d8190b13254ef0e2c545d7f7d8ed4a2f75
andrewcgaitskell <andrew@gaitskell.com>
2020-10-11 10:46:36 +0100

    Update 3_environment.sh

--- scripts/3_environment.sh
+++ scripts/3_environment.sh
@@ -11,6 +11,7 @@ pip3 install -U ipyleaflet
 pip3 install -U geopandas
 pip3 install -U seaborn
 pip3 install -U descartes
+pip3 install -U shapefile
 
 pip3 install -U jupyterhub
 pip3 install -U jupyterlab

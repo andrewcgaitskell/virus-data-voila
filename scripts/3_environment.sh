f0f2da04fa8c8d25f066041fbbf21f264551fcda
andrewcgaitskell <andrew@gaitskell.com>
2020-10-11 12:48:20 +0100

    Update 3_environment.sh

--- scripts/3_environment.sh
+++ scripts/3_environment.sh
@@ -17,6 +17,17 @@ pip3 install -U jupyterhub
 pip3 install -U jupyterlab
 ##pip3 install -U notebook
 
+########## to install basemap ###### from here
+
+apt install libgeos-3.6.2
+apt install libgeos-dev
+apt install libgeos-c1v5
+
+pip3 install https://github.com/matplotlib/basemap/archive/master.zip
+
+######## to here ########
+
+
 pip3 install -U wheel
 
 pip3 install -U ipywidgets

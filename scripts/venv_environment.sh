7b293da9857f4f9f56fbec6605e822c9f3b363a7
andrewcgaitskell <andrew@gaitskell.com>
2020-10-12 13:32:56 +0100

    Update venv_environment.sh

--- scripts/venv_environment.sh
+++ scripts/venv_environment.sh
@@ -38,5 +38,5 @@ jupyter contrib nbextension install --sys-prefix  # Kinda important
 pip install jupyter_nbextensions_configurator
 jupyter nbextensions_configurator enable --sys-prefix
 python -m ipykernel install --user --name=env
-jupyter nbextension enable --py widgetsnbextension
-##jupyter labextension install @jupyter-widgets/jupyterlab-manager
+jupyter nbextension enable --py widgetsnbextension --sys-prefix
+jupyter labextension install @jupyter-widgets/jupyterlab-manager

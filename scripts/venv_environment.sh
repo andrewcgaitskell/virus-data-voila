6b4622479660f606612718eeb3bfe30576392d60
andrewcgaitskell <andrew@gaitskell.com>
2020-10-12 12:45:48 +0100

    Create venv_environment.sh

---
+++ scripts/venv_environment.sh
@@ -0,0 +1,37 @@
+pip install -U wget
+pip install -U sqlalchemy
+pip install -U psycopg2
+pip install -U networkx
+pip install -U ipympl
+pip install -U scipy
+pip install -U voila
+pip install -U bqplot
+pip install -U ipyvolume
+pip install -U ipyleaflet
+pip install -U geopandas
+pip install -U seaborn
+pip install -U descartes
+pip install -U PyShp
+
+pip install -U jupyterhub
+pip install -U jupyterlab
+##pip install -U notebook
+
+########## to install basemap ###### from here
+
+sudo apt install libgeos-3.6.2
+sudo apt install libgeos-dev
+sudo apt install libgeos-c1v5
+
+pip install https://github.com/matplotlib/basemap/archive/master.zip
+
+######## to here ########
+
+
+pip install -U wheel
+
+pip install -U ipywidgets
+
+jupyter nbextension enable --py widgetsnbextension
+
+jupyter labextension install @jupyter-widgets/jupyterlab-manager

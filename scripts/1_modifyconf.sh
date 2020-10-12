da8019ea661258b3ff41f02e25014d41189313da
andrewcgaitskell <andrew@gaitskell.com>
2020-10-12 09:33:31 +0100

    Update 1_modifyconf.sh

--- scripts/1_modifyconf.sh
+++ scripts/1_modifyconf.sh
@@ -2,6 +2,6 @@
 ##sudo echo "host all  all    0.0.0.0/0 md5" >> /etc/postgresql/10/main/pg_hba.conf
 ##sudo echo "listen_addresses='*'" >> /etc/postgresql/10/main/postgresql.conf
 
-sed 's+host    all             all             127.0.0.1/32            md5+host    all             all             0.0.0.0/0            md5+' /etc/postgresql/10/main/pg_hba.conf > /etc/postgresql/10/main/pg_hba_new.conf
+sudo sed 's+host    all             all             127.0.0.1/32            md5+host    all             all             0.0.0.0/0            md5+' /etc/postgresql/10/main/pg_hba.conf > /etc/postgresql/10/main/pg_hba_new.conf
 
-sed 's+#listen_addresses = '\''localhost'\''         # what IP address(es) to listen on;+listen_addresses = '\''*'\''         # what IP address(es) to listen on;+' /etc/postgresql/10/main/postgresql.conf > /etc/postgresql/10/main/postgresql_new.conf
+sudo sed 's+#listen_addresses = '\''localhost'\''         # what IP address(es) to listen on;+listen_addresses = '\''*'\''         # what IP address(es) to listen on;+' /etc/postgresql/10/main/postgresql.conf > /etc/postgresql/10/main/postgresql_new.conf

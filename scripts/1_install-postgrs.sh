faf308a9cab43cd452edeb2303e464b5931ec179
andrewcgaitskell <andrew@gaitskell.com>
2020-10-12 09:45:27 +0100

    Update 1_install-postgrs.sh

--- scripts/1_install-postgrs.sh
+++ scripts/1_install-postgrs.sh
@@ -15,12 +15,7 @@ psqluser="pythonuser"   # Database username
 psqlpass="pythonuser"  # Database password
 psqldb="data"   # Database name
 
-printf "CREATE ROLE pythonuser WITH LOGIN SUPERUSER INHERIT CREATEDB CREATEROLE NOREPLICATION PASSWORD 'pythonuser';\nCREATE DATABASE $psqldb WITH OWNER $psqluser;" > createuserdb.sql
+sudo printf "CREATE ROLE pythonuser WITH LOGIN SUPERUSER INHERIT CREATEDB CREATEROLE NOREPLICATION PASSWORD 'pythonuser';\nCREATE DATABASE $psqldb WITH OWNER $psqluser;" > createuserdb.sql
 
 sudo -u postgres psql -f createuserdb.sql
 
-sudo echo "host all  all    0.0.0.0/0 md5" >> /etc/postgresql/10/main/pg_hba.conf
-
-sudo echo "listen_addresses='*'" >> /etc/postgresql/10/main/postgresql.conf
-
-sudo service postgresql restart

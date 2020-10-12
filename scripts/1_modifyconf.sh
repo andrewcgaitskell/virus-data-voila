ce05fae09bdeac3f850c2c5d2530ec378719fb77
andrewcgaitskell <andrew@gaitskell.com>
2020-10-12 09:15:16 +0100

    Update 1_modifyconf.sh

--- scripts/1_modifyconf.sh
+++ scripts/1_modifyconf.sh
@@ -2,10 +2,7 @@
 ##sudo echo "host all  all    0.0.0.0/0 md5" >> /etc/postgresql/10/main/pg_hba.conf
 ##sudo echo "listen_addresses='*'" >> /etc/postgresql/10/main/postgresql.conf
 
-
-sed 's/UK/GB/' test.txt > newfile.txt
-
-sed 's+127.0.0.1/32+0.0.0.0/0+' /etc/postgresql/10/main/pg_hba.conf > /etc/postgresql/10/main/pg_hba_new.conf
+sed 's+host    all             all             127.0.0.1/32            md5+host    all             all             0.0.0.0/0            md5+' /etc/postgresql/10/main/pg_hba.conf > /etc/postgresql/10/main/pg_hba_new.conf
 
 
 #Edit note name with double quotation e.g. "webserver01".

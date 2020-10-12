e30999e66dc654e0027bc9c5c81375aae2ae7d7b
jupyterlab <jupyterlab@alldancing-vm-6.europe-west2-c.c.voila20200530.internal>
2020-10-12 13:51:44 +0000

    added/corrected new sql files

--- notebooks/Get/CreateValidationFunctions.sql
+++ notebooks/Get/CreateValidationFunctions.sql
@@ -48,25 +48,3 @@ end;$BODY$;
 
 ALTER FUNCTION public.is_valid_int(text)
     OWNER TO pythonuser;
-
-DROP TABLE if exists public."raw_All_OWID_Data";
-
-CREATE TABLE public."raw_All_OWID_Data"
-(
-    index bigint,
-    iso_code text COLLATE pg_catalog."default",
-    continent text COLLATE pg_catalog."default",
-    "Country" text COLLATE pg_catalog."default",
-    "date" text COLLATE pg_catalog."default",
-    week text COLLATE pg_catalog."default",
-    variable text COLLATE pg_catalog."default",
-    value text COLLATE pg_catalog."default"
-)
-WITH (
-    OIDS = FALSE
-)
-TABLESPACE pg_default;
-
-ALTER TABLE public."raw_All_OWID_Data"
-    OWNER to pythonuser;
--- Index: ix_tbl_All_OWID_Data_index
--- scripts/4_load_data.sh
+++ scripts/4_load_data.sh
@@ -1,7 +1,7 @@
-jupyter nbconvert --ExecutePreprocessor.timeout=-1 --to notebook --execute /home/jupyterlab/voila_current/notebooks/Get/CovidTrackerDataGanttWget.ipynb
-
 jupyter nbconvert --ExecutePreprocessor.timeout=-1 --to notebook --execute /home/jupyterlab/voila_current/notebooks/Get/OWIDCovidDataMeasuresWget.ipynb
 
 jupyter nbconvert --ExecutePreprocessor.timeout=-1 --to notebook --execute /home/jupyterlab/voila_current/notebooks/Get/OxCGRT_latestWget.ipynb
 
 jupyter nbconvert --ExecutePreprocessor.timeout=-1 --to notebook --execute /home/jupyterlab/voila_current/notebooks/Get/ReshapeTimeSeries2Database.ipynb
+
+jupyter nbconvert --ExecutePreprocessor.timeout=-1 --to notebook --execute /home/jupyterlab/voila_current/notebooks/Get/CovidTrackerDataGanttWget.ipynb

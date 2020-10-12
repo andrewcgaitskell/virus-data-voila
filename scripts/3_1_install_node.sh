14720721ae9f72ae30b67a08213f47809b96780a
andrewcgaitskell <andrew@gaitskell.com>
2020-10-12 13:53:26 +0100

    Update 3_1_install_node.sh

--- scripts/3_1_install_node.sh
+++ scripts/3_1_install_node.sh
@@ -2,4 +2,8 @@
 sudo curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 sudo apt install -y nodejs
 
+curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt update && sudo apt install yarn
+
 sudo npm install -g configurable-http-proxy

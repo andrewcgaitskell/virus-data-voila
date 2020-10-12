5f7503d2e9475b892dfc4dba37ef1453e10e83f0
andrewcgaitskell <andrew@gaitskell.com>
2020-10-12 14:06:34 +0100

    Update 4_load_data.sh

--- scripts/4_load_data.sh
+++ scripts/4_load_data.sh
@@ -1,7 +1,7 @@
+sudo jupyter nbconvert --ExecutePreprocessor.timeout=-1 --to notebook --execute /home/jupyterlab/voila_current/notebooks/Get/CovidTrackerDataGanttWget.ipynb
+
 sudo jupyter nbconvert --ExecutePreprocessor.timeout=-1 --to notebook --execute /home/jupyterlab/voila_current/notebooks/Get/OWIDCovidDataMeasuresWget.ipynb
 
 sudo jupyter nbconvert --ExecutePreprocessor.timeout=-1 --to notebook --execute /home/jupyterlab/voila_current/notebooks/Get/OxCGRT_latestWget.ipynb
 
 sudo jupyter nbconvert --ExecutePreprocessor.timeout=-1 --to notebook --execute /home/jupyterlab/voila_current/notebooks/Get/ReshapeTimeSeries2Database.ipynb
-
-sudo jupyter nbconvert --ExecutePreprocessor.timeout=-1 --to notebook --execute /home/jupyterlab/voila_current/notebooks/Get/CovidTrackerDataGanttWget.ipynb

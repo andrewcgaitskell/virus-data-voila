e43b55c131d08adf584e69ef4a33a2958a900528
andrewcgaitskell <andrew@gaitskell.com>
2020-10-12 09:22:04 +0100

    Update 1_modifyconf.sh

--- scripts/1_modifyconf.sh
+++ scripts/1_modifyconf.sh
@@ -4,6 +4,7 @@
 
 sed 's+host    all             all             127.0.0.1/32            md5+host    all             all             0.0.0.0/0            md5+' /etc/postgresql/10/main/pg_hba.conf > /etc/postgresql/10/main/pg_hba_new.conf
 
+sed 's+#listen_addresses = 'localhost'         # what IP address(es) to listen on;+s+#listen_addresses = '*'         # what IP address(es) to listen on;+' /etc/postgresql/10/main/postgresql.conf > /etc/postgresql/10/main/postgresql_new.conf
 
 #Edit note name with double quotation e.g. "webserver01".
 newIP="0.0.0.0/0"

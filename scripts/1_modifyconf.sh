3fedc6054aee4d826d78beaf2aac1dcc7ce86311
andrewcgaitskell <andrew@gaitskell.com>
2020-10-12 09:03:51 +0100

    Update 1_modifyconf.sh

--- scripts/1_modifyconf.sh
+++ scripts/1_modifyconf.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
+##sudo echo "host all  all    0.0.0.0/0 md5" >> /etc/postgresql/10/main/pg_hba.conf
+##sudo echo "listen_addresses='*'" >> /etc/postgresql/10/main/postgresql.conf
+
+
+sed 's/UK/GB/' test.txt > newfile.txt
+
+sed 's/127.0.0.1/0.0.0.0/' /etc/postgresql/10/main/pg_hba.conf > /etc/postgresql/10/main/pg_hba_new.conf
+
 
 #Edit note name with double quotation e.g. "webserver01".
-newNode="server211"
+newIP="0.0.0.0/0"
 
 #Set OpenCanary Configuration File Path
-openCanaryConfig="info.conf"
+openCanaryConfig="/etc/postgresql/10/main/pg_hba.conf"
 sed -i 's/\(^"device.node_id":\).*/\1 "'$newNode'",/' $openCanaryConfig

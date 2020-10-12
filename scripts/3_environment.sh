625709debcadf4f87f9e0bbc96d59fcb94549aa3
andrewcgaitskell <andrew@gaitskell.com>
2020-10-12 09:53:51 +0100

    Update 3_environment.sh

--- scripts/3_environment.sh
+++ scripts/3_environment.sh
@@ -1,38 +1,38 @@
-pip3 install -U wget
-pip3 install -U sqlalchemy
-pip3 install -U psycopg2
-pip3 install -U networkx
-pip3 install -U ipympl
-pip3 install -U scipy
-pip3 install -U voila
-pip3 install -U bqplot
-pip3 install -U ipyvolume
-pip3 install -U ipyleaflet
-pip3 install -U geopandas
-pip3 install -U seaborn
-pip3 install -U descartes
-pip3 install -U PyShp
+sudo pip3 install -U wget
+sudo pip3 install -U sqlalchemy
+sudo pip3 install -U psycopg2
+sudo pip3 install -U networkx
+sudo pip3 install -U ipympl
+sudo pip3 install -U scipy
+sudo pip3 install -U voila
+sudo pip3 install -U bqplot
+sudo pip3 install -U ipyvolume
+sudo pip3 install -U ipyleaflet
+sudo pip3 install -U geopandas
+sudo pip3 install -U seaborn
+sudo pip3 install -U descartes
+sudo pip3 install -U PyShp
 
-pip3 install -U jupyterhub
-pip3 install -U jupyterlab
+sudo pip3 install -U jupyterhub
+sudo pip3 install -U jupyterlab
 ##pip3 install -U notebook
 
 ########## to install basemap ###### from here
 
-apt install libgeos-3.6.2
-apt install libgeos-dev
-apt install libgeos-c1v5
+sudo apt install libgeos-3.6.2
+sudo apt install libgeos-dev
+sudo apt install libgeos-c1v5
 
-pip3 install https://github.com/matplotlib/basemap/archive/master.zip
+sudo pip3 install https://github.com/matplotlib/basemap/archive/master.zip
 
 ######## to here ########
 
 
-pip3 install -U wheel
+sudo pip3 install -U wheel
 
-pip3 install -U ipywidgets
+sudo pip3 install -U ipywidgets
 
-jupyter nbextension enable --py widgetsnbextension
+sudo jupyter nbextension enable --py widgetsnbextension
 
-jupyter labextension install @jupyter-widgets/jupyterlab-manager
+sudo jupyter labextension install @jupyter-widgets/jupyterlab-manager
 

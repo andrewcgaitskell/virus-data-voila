201eee34b3430e53f0ccba221b172dbc76d9d1f4
jupyterlab <jupyterlab@alldancing-vm-6.europe-west2-c.c.voila20200530.internal>
2020-10-15 11:10:56 +0000

    lod now working

---
+++ notebooks/Maps/CreateLSOAHierarchyAndNames.sql
@@ -0,0 +1,154 @@
+drop table if exists public.ref_lsoa_hierarchy;
+
+SELECT distinct
+--"OA11CD",
+--"OAC11CD",
+--"OAC11NM",
+"LSOA11CD",
+--"LSOA11NM",
+--"SOAC11CD",
+--"SOAC11NM",
+"MSOA11CD",
+--"MSOA11NM",
+"LAD17CD",
+--"LAD17NM",
+--"LACCD",
+--"LACNM",
+"RGN11CD",
+--"RGN11NM",
+"CTRY11CD"--,
+--"CTRY11NM"--,
+--"FID"
+into public.ref_lsoa_hierarchy
+FROM public.data_lsoalookup;
+
+-- lsoa names
+
+drop table if exists public.ref_lsoa_names;
+
+SELECT distinct
+--"OA11CD",
+--"OAC11CD",
+--"OAC11NM",
+"LSOA11CD" as code,
+"LSOA11NM" as name
+--"SOAC11CD",
+--"SOAC11NM",
+--"MSOA11CD",
+--"MSOA11NM",
+--"LAD17CD",
+--"LAD17NM",
+--"LACCD",
+--"LACNM",
+--"RGN11CD",
+--"RGN11NM",
+--"CTRY11CD"--,
+--"CTRY11NM"--,
+--"FID"
+into public.ref_lsoa_names
+FROM public.data_lsoalookup;
+
+-- msoa names
+
+drop table if exists public.ref_msoa_names;
+
+SELECT distinct
+--"OA11CD",
+--"OAC11CD",
+--"OAC11NM",
+--"LSOA11CD",
+--"LSOA11NM",
+--"SOAC11CD",
+--"SOAC11NM",
+"MSOA11CD" as code,
+"MSOA11NM" as name
+--"LAD17CD",
+--"LAD17NM",
+--"LACCD",
+--"LACNM",
+--"RGN11CD",
+--"RGN11NM",
+--"CTRY11CD"--,
+--"CTRY11NM"--,
+--"FID"
+into public.ref_msoa_names
+FROM public.data_lsoalookup;
+
+
+-- lad names
+
+drop table if exists public.ref_lad_names;
+
+SELECT distinct
+--"OA11CD",
+--"OAC11CD",
+--"OAC11NM",
+--"LSOA11CD",
+--"LSOA11NM",
+--"SOAC11CD",
+--"SOAC11NM",
+--"MSOA11CD" ,
+--"MSOA11NM" ,
+"LAD17CD" as code,
+"LAD17NM" as name
+--"LACCD",
+--"LACNM",
+--"RGN11CD",
+--"RGN11NM",
+--"CTRY11CD"--,
+--"CTRY11NM"--,
+--"FID"
+into public.ref_lad_names
+FROM public.data_lsoalookup;
+
+-- rgn names
+
+drop table if exists public.ref_rgn_names;
+
+SELECT distinct
+--"OA11CD",
+--"OAC11CD",
+--"OAC11NM",
+--"LSOA11CD",
+--"LSOA11NM",
+--"SOAC11CD",
+--"SOAC11NM",
+--"MSOA11CD" ,
+--"MSOA11NM" ,
+--"LAD17CD" ,
+--"LAD17NM" ,
+--"LACCD" ,
+--"LACNM" ,
+"RGN11CD" as code,
+"RGN11NM" as name
+--"CTRY11CD",
+--"CTRY11NM",
+--"FID"
+into public.ref_rgn_names
+FROM public.data_lsoalookup;
+
+-- ctry names
+
+drop table if exists public.ref_ctry_names;
+
+SELECT distinct
+--"OA11CD",
+--"OAC11CD",
+--"OAC11NM",
+--"LSOA11CD",
+--"LSOA11NM",
+--"SOAC11CD",
+--"SOAC11NM",
+--"MSOA11CD" ,
+--"MSOA11NM" ,
+--"LAD17CD" ,
+--"LAD17NM" ,
+--"LACCD" ,
+--"LACNM" ,
+--"RGN11CD",
+--"RGN11NM",
+"CTRY11CD" as code,
+"CTRY11NM" as name
+--"FID"
+into public.ref_ctry_names
+FROM public.data_lsoalookup;

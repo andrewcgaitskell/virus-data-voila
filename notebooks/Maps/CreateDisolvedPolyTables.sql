9a6bc17b4539267c23cc976194810958d9f2c320
andrewcgaitskell <andrew@gaitskell.com>
2020-10-18 17:34:35 +0100

    Update CreateDisolvedPolyTables.sql

--- notebooks/Maps/CreateDisolvedPolyTables.sql
+++ notebooks/Maps/CreateDisolvedPolyTables.sql
@@ -4,23 +4,31 @@ drop table if exists public.poly_lad;
 drop table if exists public.poly_rgn;
 drop table if exists public.poly_ctry;
 
+--select distinct "LAD17CD" from public.data_ew_lsoapolys l
 
-SELECT l."LSOA11CD" as code, ST_UnaryUnion(ST_Collect(l.valid_geom)) geometry into public.poly_lsoa
-	FROM public.data_ew_lsoapolys l
-	group by l."LSOA11CD";
+SELECT l."MSOA11CD" as code,
+ST_MakePolygon(ST_ExteriorRing((ST_Dump(ST_Union(valid_geom))).geom)) as geometry
+into public.poly_msoa
+FROM public.data_ew_lsoapolys l
+group by l."MSOA11CD";
 
-SELECT l."MSOA11CD" as code, ST_UnaryUnion(ST_Collect(l.valid_geom)) geometry  into public.poly_msoa
-	FROM public.data_ew_lsoapolys l
-	group by l."MSOA11CD";
+SELECT
+"LAD17CD" as code,
+ST_MakePolygon(ST_ExteriorRing((ST_Dump(ST_Union(valid_geom))).geom)) as geometry
+into public.poly_lad
+FROM public.data_ew_lsoapolys
+group by "LAD17CD";
 
-SELECT l."LAD17CD" as code, ST_UnaryUnion(ST_Collect(l.valid_geom)) geometry into public.poly_lad
-	FROM public.data_ew_lsoapolys l
-	group by l."LAD17CD";
+SELECT
+"RGN11CD" as code,
+ST_MakePolygon(ST_ExteriorRing((ST_Dump(ST_Union(valid_geom))).geom)) as geometry
+into public.poly_rgn
+FROM public.data_ew_lsoapolys
+group by "RGN11CD";
 
-SELECT l."RGN11CD" as code, ST_UnaryUnion(ST_Collect(l.valid_geom)) geometry into public.poly_rgn
-	FROM public.data_ew_lsoapolys l
-	group by l."RGN11CD";
-
-SELECT l."CTRY11CD" as code, ST_UnaryUnion(ST_Collect(l.valid_geom)) geometry into public.poly_ctry
-	FROM public.data_ew_lsoapolys l
-	group by l."CTRY11CD";
+SELECT
+"CTRY11CD" as code,
+ST_MakePolygon(ST_ExteriorRing((ST_Dump(ST_Union(valid_geom))).geom)) as geometry
+into public.poly_ctry
+FROM public.data_ew_lsoapolys
+group by "CTRY11CD";

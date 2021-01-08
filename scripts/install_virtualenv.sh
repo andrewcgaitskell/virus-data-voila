dfd5df4e4c31f6f105a78b726f811261902f0465
andrewcgaitskell <andrew@gaitskell.com>
2021-01-08 14:07:23 +0000

    Update install_virtualenv.sh

--- scripts/install_virtualenv.sh
+++ scripts/install_virtualenv.sh
@@ -1,11 +1,11 @@
-all run as jupyterlab user
+all run as webuser user
 
 pip3 install virtualenv
 
 python -m venv env
 
-source /home/jupyterlab/env/bin/activate
+source /var/www/virus-data-voila/env/bin/activate
 
-cd /home/ubuntu/voila/scripts
+cd /var/www/virus-data-voila/scripts
 
 bash venv_environment.sh

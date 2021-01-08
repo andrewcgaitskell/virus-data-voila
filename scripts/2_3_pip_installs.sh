6c1c6173ab030b94323b26461c701b2c392b836f
andrewcgaitskell <andrew@gaitskell.com>
2021-01-08 15:27:15 +0000

    Create 2_3_pip_installs.sh

---
+++ scripts/2_3_pip_installs.sh
@@ -0,0 +1,37 @@
+pip3 install -U wget
+pip3 install -U sqlalchemy
+sudo pip3 install -U psycopg2
+sudo pip3 install -U networkx
+sudo pip3 install -U ipympl
+sudo pip3 install -U scipy
+sudo pip3 install -U voila
+sudo pip3 install -U bqplot
+sudo pip3 install -U ipyvolume
+sudo pip3 install -U ipyleaflet
+sudo pip3 install -U geopandas
+sudo pip3 install -U seaborn
+sudo pip3 install -U descartes
+sudo pip3 install -U PyShp
+
+sudo pip3 install -U jupyterhub
+sudo pip3 install -U jupyterlab
+##pip3 install -U notebook
+
+########## to install basemap ###### from here
+
+sudo apt install libgeos-3.6.2
+sudo apt install libgeos-dev
+sudo apt install libgeos-c1v5
+
+sudo pip3 install https://github.com/matplotlib/basemap/archive/master.zip
+
+######## to here ########
+
+
+sudo pip3 install -U wheel
+
+sudo pip3 install -U ipywidgets
+
+sudo jupyter nbextension enable --py widgetsnbextension
+
+sudo jupyter labextension install @jupyter-widgets/jupyterlab-manager

cb51c98c4afa29a86744e2ee4719e50288399f68
andrewcgaitskell <andrew@gaitskell.com>
2021-01-08 13:48:38 +0000

    Create install.sh

---
+++ scripts/python/install.sh
@@ -0,0 +1,13 @@
+sudo apt install build-essential -y
+
+sudo apt install python-software-properties -y
+
+sudo apt install software-properties-common -y
+
+sudo apt install python3 -y
+sudo apt install python3-dev -y
+sudo apt install libpq-dev -y
+
+sudo apt install python3-pip -y
+
+sudo apt install python3-venv -y

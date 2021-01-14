f58380256baae47ed4ef004670d91ea5db132fa7
andrewcgaitskell <andrew@gaitskell.com>
2021-01-14 16:34:06 +0000

    Update 4_load_data.sh

--- scripts/4_load_data.sh
+++ scripts/4_load_data.sh
@@ -1,7 +1,7 @@
-sudo jupyter nbconvert --ExecutePreprocessor.timeout=-1 --to notebook --execute /home/webuser/virus-data-voila/notebooks/Get/OWIDCovidDataMeasuresWget.ipynb
+jupyter nbconvert --ExecutePreprocessor.timeout=-1 --to notebook --execute /home/webuser/virus-data-voila/notebooks/Get/OWIDCovidDataMeasuresWget.ipynb
 
-sudo jupyter nbconvert --ExecutePreprocessor.timeout=-1 --to notebook --execute /home/webuser/virus-data-voila/notebooks/Get/OxCGRT_latestWget.ipynb
+jupyter nbconvert --ExecutePreprocessor.timeout=-1 --to notebook --execute /home/webuser/virus-data-voila/notebooks/Get/OxCGRT_latestWget.ipynb
 
-sudo jupyter nbconvert --ExecutePreprocessor.timeout=-1 --to notebook --execute /home/webuser/virus-data-voila/notebooks/Get/ReshapeTimeSeries2Database.ipynb
+jupyter nbconvert --ExecutePreprocessor.timeout=-1 --to notebook --execute /home/webuser/virus-data-voila/notebooks/Get/ReshapeTimeSeries2Database.ipynb
 
-sudo jupyter nbconvert --ExecutePreprocessor.timeout=-1 --to notebook --execute /home/webuser/virus-data-voila/notebooks/Get/CovidTrackerDataGanttWget.ipynb
+jupyter nbconvert --ExecutePreprocessor.timeout=-1 --to notebook --execute /home/webuser/virus-data-voila/notebooks/Get/CovidTrackerDataGanttWget.ipynb

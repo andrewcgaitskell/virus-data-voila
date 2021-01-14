52feb56ba0eb6e3c46d53a9b0a34183a24643440
andrewcgaitskell <andrew@gaitskell.com>
2021-01-14 11:09:43 +0000

    Update jupyterhub_config.py

--- jupyterhub/jupyterhub_config.py
+++ jupyterhub/jupyterhub_config.py
@@ -868,7 +868,7 @@ c.Spawner.notebook_dir = '/'
 #  Admin access should be treated the same way root access is.
 #  
 #  Defaults to an empty set, in which case no user has admin access.
-c.Authenticator.admin_users = {'sweep2oo','sweep0ne','jupyterlab'}
+c.Authenticator.admin_users = {'jupyterlab','webuser'}
 
 ## The max age (in seconds) of authentication info before forcing a refresh of
 #  user auth info.
